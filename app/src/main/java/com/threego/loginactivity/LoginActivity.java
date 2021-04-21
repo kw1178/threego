694daacf657d60dad4f494c6aa7ebd41c24dd5e9
21SMT01 <lsb3051@gmail.com>
2021-04-21 14:59:25 +0900

    창수시

--- app/src/main/java/com/threego/loginactivity/LoginActivity.java
+++ app/src/main/java/com/threego/loginactivity/LoginActivity.java
@@ -11,7 +11,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btn_join, btn_login, btn_exit;
+    Button btn_join, btn_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_join = findViewById(R.id.btn_join);
         btn_login = findViewById(R.id.btn_login);
-        btn_exit = findViewById(R.id.btn_exit);
+
 
         // 회원가입
         btn_join.setOnClickListener(new View.OnClickListener() {    // startActivityForResult 해야한다.
@@ -40,27 +41,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // 회원탈퇴
-        btn_exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 다이얼로그 띄우기
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                builder.setTitle("삭제하기");
-                builder.setMessage("삭제하시겠습니까?");
-                // PositiveButton ->오른쪽 버튼, NegativeButton -> 왼쪽 버튼
 
-                // 문구, 버튼 클릭시 일어나는 일
-                builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(),"다음에도 이용해주세요. ^^",Toast.LENGTH_SHORT).show();
-                    }
-                });
-                // 할일 없으면 null
-                builder.setNegativeButton("cancle", null);
-                builder.show();
-            }
-        });
     }
 }
