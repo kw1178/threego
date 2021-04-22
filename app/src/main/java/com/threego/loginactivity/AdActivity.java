4906b715443f4f0e9dc9b81b8079176feba61243
jamongtart <hkkim121@gmail.com>
2021-04-22 12:31:19 +0900

    0422 라파연동, 수익업데이트 완료

--- app/src/main/java/com/threego/loginactivity/AdActivity.java
+++ app/src/main/java/com/threego/loginactivity/AdActivity.java
@@ -1,5 +1,6 @@
 package com.threego.loginactivity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -19,18 +20,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.dinuscxj.progressbar.CircleProgressBar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static androidx.core.view.GravityCompat.START;
 
@@ -38,13 +43,14 @@ public class AdActivity extends AppCompatActivity {
 
     //adRequestQueue, adStringRequest 일반광고
     // vidRequestQueue, vidStringRequest 영상광고
-    RequestQueue adReq, vidReq;
-    StringRequest adStrReq, vidStrReq;
+    RequestQueue adReq, vidReq, moneyReq;
+    StringRequest adStrReq, vidStrReq, moneyStrReq;
 
     ImageView iv_menu, iv_gif;
     ImageButton ibtn_close;
     TextView tv_money, tv_ad_name, tv_time;
     ProgressBar pro;
+    CircleProgressBar circle;
     VideoView videoView;
     MediaController mediaController;
     //String videoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"; // 경로, 확장자까지 필요
@@ -66,12 +72,13 @@ public class AdActivity extends AppCompatActivity {
         tv_time = findViewById(R.id.tv_time);
 
         //pro = findViewById(R.id.pro);
+        circle = findViewById(R.id.circle);
 
         videoView = (VideoView) findViewById(R.id.videoView);
         //iv_gif = findViewById(R.id.imageView4);
         //Glide.with(this).load("https://media4.giphy.com/media/K4x1ZL36xWCf6/giphy.gif").into(iv_gif);
 
-        btn_change = findViewById(R.id.btn_change);
+        //btn_change = findViewById(R.id.btn_change);
 
         iv_menu = findViewById(R.id.iv_menu);
         ibtn_close = findViewById(R.id.ibtn_close);
@@ -143,20 +150,58 @@ public class AdActivity extends AppCompatActivity {
 
         // 영상광고 연동
         //tv_ad_name.setText("BURGER KING 기네스 와퍼");
-        tv_money.setText("2350");
+        //tv_money.setText("2350");
 
         vidReq = Volley.newRequestQueue(getApplicationContext());
         String vidUrl = "http://222.102.104.235:8081/threego/appvid.do";
 
-        vidStrReq = new StringRequest(Request.Method.GET, vidUrl, new Response.Listener<String>() {
+        vidStrReq = new StringRequest(Request.Method.POST, vidUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 //Log.d("test", response.toString());
-                int max = 0;
                 try {
                     JSONObject obj = new JSONObject(response);
                     tv_ad_name.setText(obj.getString("a_contents")+" : "+obj.getString("a_name"));
-                    tv_time.setText(obj.getString("a_time"));
+                    tv_time.setText(obj.getInt("a_time")+" / 100회");
+                    tv_money.setText(String.valueOf((100-obj.getInt("a_time"))*60));
+                    circle.setProgress(100-obj.getInt("a_time"));
+                    String rider = obj.getString("r_id");
+
+                    // 광고 횟수 0이 되면 예치금으로 업데이트되는 통신
+                    if(obj.getInt("a_time")==0 && tv_money.getText().toString()!="0"){
+                        moneyReq = Volley.newRequestQueue(getApplicationContext());
+                        String moneyUrl = "http://222.102.104.235:8081/threego/moneyUpdate.do";
+
+                        moneyStrReq = new StringRequest(Request.Method.POST, moneyUrl, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                //Log.d("test", response.toString());
+                                if(response != null){
+                                    Toast.makeText(getApplicationContext(), "예치금으로 전환되었습니다!", Toast.LENGTH_SHORT).show();
+                                    tv_money.setText("0");
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "예치금 전환 실패!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }){
+                            @Nullable
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> temp = new HashMap<>();
+                                temp.put("a_money", tv_money.getText().toString());
+                                temp.put("r_id", rider);
+                                return temp;
+                            }
+                        };
+                        moneyReq.add(moneyStrReq);
+
+                    } //_moneyReq
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -231,13 +276,14 @@ public class AdActivity extends AppCompatActivity {
         adReq.add(adStrReq);
 
 
+        // 이 버튼 일단 봉인...
         // 예치금 전환 버튼 클릭시 Toast 메시지 띄우기
-        btn_change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "예치금이 전환되었습니다. ^^", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        btn_change.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "예치금이 전환되었습니다. ^^", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         // 네비게이션 열고 닫기
         iv_menu.setOnClickListener(new View.OnClickListener() {
